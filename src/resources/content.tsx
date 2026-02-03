95876ad54594646079a89876b41d8fe9ca382269
itsMallix <amandamaulana00@gmail.com>
2026-02-03 09:06:56 +0700

    add new avatar

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -6,7 +6,7 @@ const person: Person = {
   lastName: "Maulana",
   name: `Amanda Maulana`,
   role: "Mobile Developer",
-  avatar: "/images/avatar.png",
+  avatar: "/images/avatar.jpg",
   email: "itsmallixdev@gmail.com",
   location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
